7d194afbe903f9c3527f8cf1e43d1fd04cc63c40
unknown <stefan.pflueglmayer@gmail.com>
2025-04-27 12:17:33 +0200

    add cec switching method

--- python/AlarmDisplayControl/main.py
+++ python/AlarmDisplayControl/main.py
@@ -47,31 +47,66 @@ def signal_handler(signum, frame):
    exit(0)  # exit here
 signal.signal(signal.SIGTERM, signal_handler)
 
+# Function to select the source of the TV (just for HDMI-CEC controll!)
+def select_tv_source(switching_method:str = "default", cec_addr:str = "0.0.0.0"):
+    if os.name == "posix":
+      if switching_method.lower() == "default":
+         return  # do nothing, because you can select the source only in cec mode
+      
+      if switching_method.lower() == "cec":
+         cmd = f"echo 'as {cec_addr}' | cec-client -s -d 1"
+
+      else:
+         logging.warning("Can not select the source, wrong TV switching method selected!")
+         return
+      
+      logging.info("TV monitor input source is selected.")
+      logging.debug(cmd)
+      os.system(cmd)
+      return
+    
+    elif os.name == "nt":
+       pass  # todo
+
 # Function to turn TV on
-def turn_tv_on(way_disp:str, switching_method:str = "default"):
+def turn_tv_on(switching_method:str = "default", way_disp:str = "wayland-0", cec_addr:str = "0.0.0.0"):
    if os.name == "posix":
       if switching_method.lower() == "default":
          cmd = f"WAYLAND_DISPLAY=\"{way_disp}\" wlr-randr --output HDMI-A-1 --on"
+
       elif switching_method.lower() == "cec":
-         pass  # todo
-      
+         cmd = f"echo 'on {cec_addr}' | cec-client -s -d 1"
+
+      else:
+         logging.warning("Wrong TV switching method selected!")
+         return
+            
       logging.info("TV monitor is switched ON.")
       logging.debug(cmd)
       os.system(cmd)
+      return
+   
    elif os.name == "nt":
       pass  # todo
 
 # Function to turn TV off
-def turn_tv_off(way_disp:str, switching_method:str = "default"):
+def turn_tv_off(switching_method:str = "default", way_disp:str = "wayland-0", cec_addr:str = "0.0.0.0"):
    if os.name == "posix":
       if switching_method.lower() == "default":
          cmd = f"WAYLAND_DISPLAY=\"{way_disp}\" wlr-randr --output HDMI-A-1 --off"
+
       elif switching_method.lower() == "cec":
-         pass  # todo
+         cmd = f"echo 'standby {cec_addr}' | cec-client -s -d 1"
+
+      else:
+         logging.warning("Wrong TV switching method selected!")
+         return
 
       logging.info("TV monitor is switched OFF.")
       logging.debug(cmd)
       os.system(cmd)
+      return
+   
    elif os.name == "nt":
       pass  # todo
 
@@ -101,7 +136,7 @@ def open_browser(browser:str, url:str, wait:float, session_type:str, disp:str) -
       elif browser.lower() == "chromium":
          cmd += f"{browser.lower()} --kiosk"
       else:
-         logging.info("No or wrong browser selected!")
+         logging.warning("No or wrong browser selected!")
          return False
 
       cmd += " --new-window"
@@ -138,9 +173,8 @@ def close_browser(browser:str):
    if os.name == "posix":
       cmd = f"pkill {browser.lower()}"
 
-      logging.info(f"{browser.capitalize()} is closed.")
+      logging.info(f"{browser.capitalize()} browser is closed.")
       logging.debug(cmd)
-
       os.system(cmd)
 
    elif os.name == "nt":
@@ -151,20 +185,24 @@ def find_display(session_type:str | None) -> str | None:
    if os.name == "posix":
       if session_type == 'x11':
          # todo find out the correct display
-         logging.warning("Use always display 0 when session type is x11!")
+         logging.warning("Founded display: 0 (always display 0 when session type is x11!)")
          return '0'
       else:
          XDG_RUNTIME_DIR = f"/run/user/{os.getuid()}"
 
          if not os.path.isdir(XDG_RUNTIME_DIR):   # check if path exists
+            logging.warning("No display founded, xdg runtime path does not exist!")
             return None
          
          for item in os.listdir(XDG_RUNTIME_DIR): # Scan for wayland display
             if item.startswith("wayland-") and len(item) == 9:
+               logging.info(f"Founded display: {item}")
                return item
             
    elif os.name == "nt":
-      pass    # todo  
+      pass    # todo
+
+   logging.warning("No display founded!")
    return None    # if no display socket found, return None
 
 # Function for searching the session
@@ -172,12 +210,16 @@ def find_session() -> str | None:
    if os.name == "posix":
       XDG_SESSION_TYPE = os.environ.get('XDG_SESSION_TYPE')
       if XDG_SESSION_TYPE:
+         logging.info(f"Founded display session: {XDG_SESSION_TYPE}")
          return XDG_SESSION_TYPE
       else:
+         logging.warning(f"No session founded, use \"wayland\" as default!")
          return 'wayland'   # return 'wayland' as default display here if the XDG_SESSION_TYPE isn't set
    
    elif os.name == "nt":
       pass    # todo
+
+   logging.warning("No session founded!")
    return None
 
 
@@ -201,11 +243,6 @@ def main():
       # find session type and display here
       session_type = find_session()
       disp = find_display(session_type)
-      if disp:
-         logging.info(f"Founded session: {session_type}, Founded display: {disp}")
-      else:
-         logging.debug(f"Founded session: {session_type}, Founded display: {disp}")
-         logging.warning("No display founded!")
 
       # try to open browser in kiosk mode
       open_browser(config['BROWSER_NAME'], config['INFOSCREEN_URL'], float(config['BROWSER_LOADING_TIME']), session_type, disp)
@@ -219,7 +256,8 @@ def main():
 
          # timing algorithm to switch the tv
          if not tv_state and not blocked and (motion_detected or ext_alarm_detected):   # switch tv on
-            turn_tv_on(disp, config['TV_SWITCHING_METHOD'])
+            turn_tv_on(config['TV_SWITCHING_METHOD'], disp)
+            select_tv_source(config['TV_SWITCHING_METHOD'])
             if to_bool(config['BROWSER_REOPEN']):
                open_browser(config['BROWSER_NAME'], config['INFOSCREEN_URL'], float(config['BROWSER_LOADING_TIME']), session_type, disp)
             tv_state = True
@@ -229,7 +267,7 @@ def main():
             start_time = current_time
 
          if tv_state and (current_time - start_time >= float(config['TV_OVERRUN_TIME'])):  # switch tv off
-            turn_tv_off(disp, config['TV_SWITCHING_METHOD'])
+            turn_tv_off(config['TV_SWITCHING_METHOD'], disp)
             tv_state = False
             blocked = True
             start_time = current_time
@@ -250,7 +288,6 @@ def main():
    finally:
       # close the browser
       close_browser(config['BROWSER_NAME'])
-      logging.info(f"{config['BROWSER_NAME'].capitalize()} browser is closing.")
    
       # gpio cleanup
       del motion_sensors
@@ -258,7 +295,7 @@ def main():
       logging.info("GPIO's cleaned up!")
 
       if not tv_state:
-         turn_tv_on(disp)   # turn on tv befor stopping
+         turn_tv_on(config['TV_SWITCHING_METHOD'], disp)   # turn on tv befor stopping
 
       logging.info("TV infoscreen programm is stopped!")
 
